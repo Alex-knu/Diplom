c080cb1f263b04549e057f5083ef6c22dee42050
Alex Rozdobudko <arozdobudko2@gmail.com>
2022-12-17 21:53:23 +0200

    add any models to db

--- ITProjectPriceCalculationManager.Core/Entities/ProgramLanguage/ProgramLanguage.cs
+++ ITProjectPriceCalculationManager.Core/Entities/ProgramLanguage/ProgramLanguage.cs
@@ -5,7 +5,9 @@ namespace ITProjectPriceCalculationManager.Core.Entities.ProgramLanguage
     public class ProgramLanguage : IBaseEntity<int>
     {
         public int Id { get; set; }
-        public string Name { get; set; }
         public int SLOC { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ProgramsParametr.ProgramsParametr> ProgramsParametrs { get; set; }
     }
 }
--- ITProjectPriceCalculationManager.Core/Entities/SubjectAreaElement/SubjectAreaElement.cs
+++ ITProjectPriceCalculationManager.Core/Entities/SubjectAreaElement/SubjectAreaElement.cs
@@ -6,8 +6,10 @@ namespace ITProjectPriceCalculationManager.Core.Entities.SubjectAreaElement
     public class SubjectAreaElement : IBaseEntity<int>
     {
         public int Id { get; set; }
+        public int Count { get; set; }
         public SubjectAreaType SubjectAreaType { get; set; }
         public DifficultyLevelsType DifficultyLevelsType { get; set; }
-        public int Count { get; set; }
+
+        public ICollection<ProgramsParametrToSubjectAreaElement.ProgramsParametrToSubjectAreaElement> ProgramsParametrToSubjectAreaElements{ get; set; }
     }
 }
