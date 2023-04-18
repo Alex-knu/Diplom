6cb28f76b7d93ac2fcd5f5db562b41f67a5cf69c
Alex Rozdobudko <80115540+Alex-knu@users.noreply.github.com>
2023-04-18 22:24:30 +0300

    add db (#9)

--- ITProjectPriceCalculationManager.ITProjectsManager.API/Infrastructure/Data/ITProjectPriceCalculationManagerDbContext.cs
+++ ITProjectPriceCalculationManager.ITProjectsManager.API/Infrastructure/Data/ITProjectPriceCalculationManagerDbContext.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using ITProjectPriceCalculationManager.ITProjectsManager.API.Core.Entities.ProgramLanguage;
+using ITProjectPriceCalculationManager.ITProjectsManager.API.Core.Entities.Application;
+using ITProjectPriceCalculationManager.ITProjectsManager.API.Core.Entities.ProgramsParametr;
+using ITProjectPriceCalculationManager.ITProjectsManager.API.Core.Entities.ProgramsParametrToSubjectAreaElement;
+using ITProjectPriceCalculationManager.ITProjectsManager.API.Core.Entities.SubjectAreaElement;
 
 namespace ITProjectPriceCalculationManager.ITProjectsManager.API.Infrastructure.Data
 {
@@ -13,9 +17,18 @@ namespace ITProjectPriceCalculationManager.ITProjectsManager.API.Infrastructure.
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfiguration(new ProgramLanguageConfiguration());
-        }
 
+            modelBuilder.ApplyConfiguration(new ApplicationConfiguration());
+            modelBuilder.ApplyConfiguration(new ProgramsParametrConfiguration());
+            modelBuilder.ApplyConfiguration(new ProgramLanguageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProgramsParametrToSubjectAreaElementConfiguration());
+            modelBuilder.ApplyConfiguration(new SubjectAreaElementConfiguration());
+        }
+        
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<ProgramsParametr> ProgramsParametrs { get; set; }
         public DbSet<ProgramLanguage> ProgramLanguages { get; set; }
+        public DbSet<ProgramsParametrToSubjectAreaElement> ProgramsParametrToSubjectAreaElements { get; set; }
+        public DbSet<SubjectAreaElement> SubjectAreaElements { get; set; }
     }
 }
--- ITProjectPriceCalculationManager.ITProjectsManager.API/Program.cs
+++ ITProjectPriceCalculationManager.ITProjectsManager.API/Program.cs
@@ -32,6 +32,17 @@ internal class Program
 
         var app = builder.Build();
 
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<ITProjectPriceCalculationManagerDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            } 
+        }
+
         // Configure the HTTP request pipeline.
         // if (app.Environment.IsDevelopment())
         // {
