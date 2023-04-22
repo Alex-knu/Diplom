fb621a038303cab36208ad3a0c656bb17b6b9a8e
Alex Rozdobudko <arozdobudko2@gmail.com>
2023-04-22 21:36:29 +0300

    add envirements

--- ITProjectPriceCalculationManager.WebApp/src/environments/environment.ts
+++ ITProjectPriceCalculationManager.WebApp/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  serveces: {
+    AuthServerUrl: 'http://localhost:5010/',
+    RouteApiUrl: 'http://localhost:5008/'
+  }
 };
 
 /*
