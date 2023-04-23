d8f469c4f9ef3fb71a741c256be4ba3d9cf789a7
Alex Rozdobudko <arozdobudko2@gmail.com>
2023-04-23 19:21:56 +0300

    add missin methods

--- ITProjectPriceCalculationManager.Router.API/Program.cs
+++ ITProjectPriceCalculationManager.Router.API/Program.cs
@@ -2,48 +2,42 @@ using System.Reflection;
 using ITProjectPriceCalculationManager.DTOModels.Settings;
 using ITProjectPriceCalculationManager.Extentions.Extentions;
 
-internal class Program
-{
-    private static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables().AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+// Add services to the container.
+
+var configuration = builder.Configuration.GetSection("RouteAPI").Get<RouteSetting>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient("ITProjectsCalculator", client =>
     {
-        var builder = WebApplication.CreateBuilder(args);
+        client.BaseAddress = new Uri(configuration.ITProjectsCalculatorAPIRoute);
+    });
+builder.Services.AddHttpClient("ITProjectsManager", client =>
+    {
+        client.BaseAddress = new Uri(configuration.ITProjectsManagerAPIRoute);
+    });
 
-        builder.Configuration.AddEnvironmentVariables().AddUserSecrets(Assembly.GetExecutingAssembly(), true);
-        // Add services to the container.
+var app = builder.Build();
 
-        var configuration = builder.Configuration.GetSection("RouteAPI").Get<RouteSetting>();
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+//app.UseHttpsRedirection();
 
-        builder.Services.AddHttpClient("ITProjectsCalculator", client =>
-            {
-                client.BaseAddress = new Uri(configuration.ITProjectsCalculatorAPIRoute);
-            });
-        builder.Services.AddHttpClient("ITProjectsManager", client =>
-            {
-                client.BaseAddress = new Uri(configuration.ITProjectsManagerAPIRoute);
-            });
+app.UseAuthorization();
 
-        var app = builder.Build();
+app.MapControllers();
 
-        // Configure the HTTP request pipeline.
-        // if (app.Environment.IsDevelopment())
-        // {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        // }
+app.AddGlobalErrorHandler();
 
-        //app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.AddGlobalErrorHandler();
-
-        app.Run();
-    }
-}
+app.Run();
