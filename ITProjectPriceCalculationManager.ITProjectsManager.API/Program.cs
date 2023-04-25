f2f8298629bc29b32f85a6b415c1d9ddf66ef24b
Alex Rozdobudko <arozdobudko2@gmail.com>
2023-04-25 22:27:43 +0300

    Revert "Merge branch 'feat/add_models_to_db' of https://github.com/Alex-knu/Diplom into feat/add_models_to_db"
    This reverts commit a816522935c11cfd7ec7f633e6176ba17887d75e, reversing
    changes made to 84b96f6891dfd0ec01bc189bb52f5b650f3194d0.

--- ITProjectPriceCalculationManager.ITProjectsManager.API/Program.cs
+++ ITProjectPriceCalculationManager.ITProjectsManager.API/Program.cs
@@ -4,58 +4,50 @@ using ITProjectPriceCalculationManager.ITProjectsManager.API.Infrastructure.Data
 using ITProjectPriceCalculationManager.ITProjectsManager.API.Infrastructure.Data.Repositories;
 using ITProjectPriceCalculationManager.ITProjectsManager.API.Core.Interfaces.Services;
 using ITProjectPriceCalculationManager.ITProjectsManager.API.Stub;
-using System.Reflection;
-using ITProjectPriceCalculationManager.DTOModels.Settings;
+using ITProjectPriceCalculationManager.Extentions.Extentions;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddDbContext<ITProjectPriceCalculationManagerDbContext>(x => x.UseNpgsql(builder.Configuration["ITProjectsManagerAPI:ConnectionString"]));
+
+// Add services to the container.
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped(typeof(IRepository<,>), typeof(BaseRepository<,>));
+builder.Services.AddScoped(typeof(IApplicationService), typeof(StubApplicationService));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
 {
-    private static void Main(string[] args)
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ITProjectPriceCalculationManagerDbContext>();
+    if (context.Database.GetPendingMigrations().Any())
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Configuration.AddEnvironmentVariables().AddUserSecrets(Assembly.GetExecutingAssembly(), true);
-
-        var configuration = builder.Configuration.GetSection("ITProjectsManager").Get<ITProjectsManagerSetting>();
-
-        // Add services to the container.
-        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-        builder.Services.AddDbContext<ITProjectPriceCalculationManagerDbContext>(x => x.UseNpgsql(configuration.ConnectionString));
-
-        builder.Services.AddScoped(typeof(IRepository<,>), typeof(BaseRepository<,>));
-        builder.Services.AddScoped(typeof(IApplicationService), typeof(StubApplicationService));
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-
-            var context = services.GetRequiredService<ITProjectPriceCalculationManagerDbContext>();
-            if (context.Database.GetPendingMigrations().Any())
-            {
-                context.Database.Migrate();
-            } 
-        }
-
-        // Configure the HTTP request pipeline.
-        // if (app.Environment.IsDevelopment())
-        // {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        // }
-
-        //app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        context.Database.Migrate();
     }
 }
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.AddGlobalErrorHandler();
+
+app.MapControllers();
+
+app.Run();
