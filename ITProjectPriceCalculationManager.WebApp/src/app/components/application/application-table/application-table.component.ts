a9291f75aa5e0f192afe9bc5b56ec82c50efbff1
Alex Rozdobudko <arozdobudko2@gmail.com>
2023-05-06 20:04:53 +0300

    finish work with application table

--- ITProjectPriceCalculationManager.WebApp/src/app/components/application/application-table/application-table.component.ts
+++ ITProjectPriceCalculationManager.WebApp/src/app/components/application/application-table/application-table.component.ts
@@ -36,60 +36,18 @@ export class ApplicationTableComponent {
     });
   }
 
-  openNew() {
-    this.application = new Application();
-    this.submitted = false;
-    this.applicationDialog = true;
-  }
-
-  deleteSelectedApplications() {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete the selected applications?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.applications = this.applications.filter(val => !this.selectedApplications.includes(val));
-        //this.selectedApplications = null;
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Applications Deleted', life: 3000 });
-      }
-    });
-  }
-
-  editApplication(application: Application) {
-    // this.application = { ...application };
-    this.applicationDialog = true;
-  }
-
-  deleteApplication(application: Application) {
-    this.confirmationService.confirm({
-      message: 'Are you sure you want to delete ' + application.id + '?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.applications = this.applications.filter(val => val.id !== application.id);
-        this.application = new Application();
-        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Application Deleted', life: 3000 });
-      }
-    });
-  }
-
-  hideDialog() {
-    this.applicationDialog = false;
-    this.submitted = false;
-  }
-
   saveApplication() {
     this.submitted = true;
 
     if (this.application.id) {
-      this.applications[this.findIndexById(this.application.id)] = this.application;
       this.baseApplicationService.single.update(this.application).subscribe(
-        () => {
-          this.messageService.add({ severity: 'success', summary: 'Командира створено', detail: 'user created' })
+        application => {
+          this.applications[this.findIndexById(application.id)] = application;
+          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Application updated' });
           this.applicationDialog = false;
         },
         error => {
-          this.messageService.add({ severity: 'error', summary: 'Командира не створено!', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
         })
     }
     else {
@@ -97,14 +55,51 @@ export class ApplicationTableComponent {
       this.application.price = 0;
       this.application.status = "New";
       this.baseApplicationService.single.create(this.application).subscribe(
-        () => {
-          this.messageService.add({ severity: 'success', summary: 'Командира створено', detail: 'user created' })
+        application => {
+          this.applications.push(application);
+          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Application created' })
           this.applicationDialog = false;
         },
         error => {
-          this.messageService.add({ severity: 'error', summary: 'Командира не створено!', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
         })
     }
+  }
+
+  editApplication(application: BaseApplication) {
+    this.application = application;
+    this.applicationDialog = true;
+  }
+
+  deleteApplication(application: Application) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + application.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        if (application.id) {
+          this.baseApplicationService.single.deleteById(application.id).subscribe(
+            application => {
+              this.applications = this.applications.filter(val => val.id !== application.id);
+              this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Application Deleted' });
+            },
+            error => {
+              this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
+            })
+        }
+      }
+    });
+  }
+
+  openNew() {
+    this.application = new BaseApplication();
+    this.submitted = false;
+    this.applicationDialog = true;
+  }
+
+  hideDialog() {
+    this.applicationDialog = false;
+    this.submitted = false;
   }
 
   findIndexById(id: number): number {
@@ -117,14 +112,5 @@ export class ApplicationTableComponent {
     }
 
     return index;
-  }
-
-  createId(): string {
-    let id = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (var i = 0; i < 5; i++) {
-      id += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return id;
   }
 }
