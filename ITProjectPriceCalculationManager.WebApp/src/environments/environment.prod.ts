ae47084b4fb3c9142dcdf7d412e957134ec4f8a5
Alex Rozdobudko <arozdobudko2@gmail.com>
2023-05-07 15:52:26 +0300

    fix env urls

--- ITProjectPriceCalculationManager.WebApp/src/environments/environment.prod.ts
+++ ITProjectPriceCalculationManager.WebApp/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   serveces: {
     webserverApiPath: 'api/',
-    AuthServerUrl: 'http://auth_server/',
-    RouteApiUrl: 'http://router.api/'
+    AuthServerUrl: 'http://localhost:5010/', //'http://auth_server/'
+    RouteApiUrl: 'http://localhost:5008/' //'http://router.api/'
   }
 };
