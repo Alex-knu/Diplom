f039380b36b26124116caf92aa17ca7df02ea279
Alex Rozdobudko <80115540+Alex-knu@users.noreply.github.com>
2023-05-13 16:22:59 +0300

    correct work with errors

--- ITProjectPriceCalculationManager.Extentions/Extentions/HttpMethodsExtention.cs
+++ ITProjectPriceCalculationManager.Extentions/Extentions/HttpMethodsExtention.cs
@@ -1,30 +1,93 @@
+using ITProjectPriceCalculationManager.Extentions.Extentions;
+using ITProjectPriceCalculationManager.Extentions.Models;
+using ITProjectPriceCalculationManager.Extentions.Models.Exceptions;
+
 namespace System.Net.Http
 {
     public static class HttpMethodsExtention
     {
         public static async Task<TResult> GetAsync<TResult>(this HttpClient client, string route)
         {
-            return await ReadFromJson<TResult>(await client.GetAsync(route));
+            var response = await client.GetAsync(route);
+
+            if(!response.StatusCode.IsSuccessStatusCode())
+            {
+                var exception = await SetError(response);
+                throw exception;
+            }
+
+            return await ReadFromJson<TResult>(response);
         }
 
         public static async Task<TResult> PostAsJsonAsync<T, TResult>(this HttpClient client, string route, T query)
         {
-            return await ReadFromJson<TResult>(await client.PostAsJsonAsync<T>(route, query));
+            var response = await client.PostAsJsonAsync<T>(route, query);
+
+            if(!response.StatusCode.IsSuccessStatusCode())
+            {
+                var exception = await SetError(response);
+                throw exception;
+            }
+
+            return await ReadFromJson<TResult>(response);
         }
 
         public static async Task<TResult> PutAsJsonAsync<T, TResult>(this HttpClient client, string route, T query)
         {
-            return await ReadFromJson<TResult>(await client.PutAsJsonAsync<T>(route, query));
+            var response = await client.PutAsJsonAsync<T>(route, query);
+
+            if(!response.StatusCode.IsSuccessStatusCode())
+            {
+                var exception = await SetError(response);
+                throw exception;
+            }
+
+            return await ReadFromJson<TResult>(response);
         }
 
         public static async Task<TResult> DeleteAsJsonAsync<T, TResult>(this HttpClient client, string route, int id)
         {
-            return await ReadFromJson<TResult>(await client.DeleteAsJsonAsync<int>(route, id));
+            var response = await client.DeleteAsJsonAsync<int>(route, id);
+
+            if(!response.StatusCode.IsSuccessStatusCode())
+            {
+                var exception = await SetError(response);
+                throw exception;
+            }
+
+            return await ReadFromJson<TResult>(response);
         }
 
         private static async Task<TResult> ReadFromJson<TResult>(HttpResponseMessage responseMessage)
         {
             return await responseMessage.Content.ReadAsAsync<TResult>();
+        }
+        
+        private static async Task<Exception> SetError(HttpResponseMessage responseMessage)
+        {
+            Exception exception;
+            ErrorModel errorMessage = await ReadFromJson<ErrorModel>(responseMessage);
+
+            switch((int)responseMessage.StatusCode)
+            {
+                case 400:
+                    exception = new BadRequestException(errorMessage.Error);
+                    break;
+                case 401:
+                    exception = new UnauthorizedAccessException(errorMessage.Error);
+                    break;
+                case 404:
+                    exception = new NotFoundException(errorMessage.Error);
+                    break;
+                case 501:
+                    exception = new NotImplementedException(errorMessage.Error);
+                    break;
+                default:
+                    exception = new Exception(errorMessage.Error);
+                    break;
+            }
+
+            return exception;
         }
     }
 }
