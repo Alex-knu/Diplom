8d64fd1f5065ac597a8561e7603636b8665ce271
Alex Rozdobudko <80115540+Alex-knu@users.noreply.github.com>
2023-05-13 13:08:04 +0300

    add token parser

---
+++ ITProjectPriceCalculationManager.Extentions/Models/UserInfo.cs
@@ -0,0 +1,9 @@
+namespace ITProjectPriceCalculationManager.Extentions.Models
+{
+    public class UserInfo
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
