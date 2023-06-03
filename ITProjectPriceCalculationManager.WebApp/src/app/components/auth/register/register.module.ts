afb3ae4df5c1ff898ac6fe359c43a402a2a06342
Alex Rozdobudko <80115540+Alex-knu@users.noreply.github.com>
2023-06-03 23:48:43 +0300

    finish registration

--- ITProjectPriceCalculationManager.WebApp/src/app/components/auth/register/register.module.ts
+++ ITProjectPriceCalculationManager.WebApp/src/app/components/auth/register/register.module.ts
@@ -7,12 +7,14 @@ import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
 import { RegisterComponent } from './register.component';
 import { RegisterRoutingModule } from './register-routing.module';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   imports: [
     CommonModule,
     RegisterRoutingModule,
     ButtonModule,
+    ToastModule,
     CheckboxModule,
     InputTextModule,
     FormsModule,
--- ITProjectPriceCalculationManager.WebApp/src/app/shared/models/user.registration.ts
+++ ITProjectPriceCalculationManager.WebApp/src/app/shared/models/user.registration.ts
@@ -1,6 +1,8 @@
 export class UserRegistration {
-  constructor(
-    public username: string,
-    public email: string,
-    public password: string) { }
+    public username: string;
+    public firstName: string;
+    public lastName: string;
+    public phoneNumber: string;
+    public email: string;
+    public password: string;
 }
