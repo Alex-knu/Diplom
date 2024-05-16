44df5c399daa5434244c92c9cb0715e7283349e7
Alex Rozdobudko <arozdobudko2@gmail.com>
2024-05-16 22:39:59 +0300

    add mapper

--- ITProjectPriceCalculationManager.EvaluatorManager.API/Core/Helpers/MappingProfile.cs
+++ ITProjectPriceCalculationManager.EvaluatorManager.API/Core/Helpers/MappingProfile.cs
@@ -1,7 +1,19 @@
 using AutoMapper;
+using ITProjectPriceCalculationManager.DTOModels.DTO;
+using ITProjectPriceCalculationManager.EvaluatorManager.API.Core.Entities.BelongingFunction;
+using ITProjectPriceCalculationManager.EvaluatorManager.API.Core.Entities.EvaluateParameter;
+using ITProjectPriceCalculationManager.EvaluatorManager.API.Core.Entities.Parameters;
+using ITProjectPriceCalculationManager.EvaluatorManager.API.Core.Entities.ParameterValue;
 
 namespace ITProjectPriceCalculationManager.EvaluatorManager.API.Core.Helpers;
 
 public class MappingProfile : Profile
 {
+    public MappingProfile()
+    {
+        CreateMap<BelongingFunctionDTO, BelongingFunction>().ReverseMap();
+        CreateMap<EvaluateParameterDTO, EvaluateParameter>().ReverseMap();
+        CreateMap<ParametersDTO, Parameters>().ReverseMap();
+        CreateMap<ParameterValueDTO, ParameterValue>().ReverseMap();
+    }
 }
