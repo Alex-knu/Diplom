dd32b65fdf5b7b9bf5283975c6037bf29db6495b
Alex Rozdobudko <arozdobudko2@gmail.com>
2024-06-08 13:00:13 +0300

    refactore same code

--- ITProjectPriceCalculationManager.WebApp/src/app/components/application/application-evaluate-paramete-info/application-evaluate-paramete-info.component.ts
+++ ITProjectPriceCalculationManager.WebApp/src/app/components/application/application-evaluate-paramete-info/application-evaluate-paramete-info.component.ts
@@ -1,13 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { BaseApplication } from 'src/app/shared/models/baseApplication.model';
 import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { UUID } from 'angular2-uuid';
-import { ProgramLanguage } from 'src/app/shared/models/programLanguage.model';
 import { Parameter } from 'src/app/shared/models/parameter.model';
 import { ParameterService } from 'src/app/shared/services/api/parameter.service';
-import {belongingFunctions} from "../../../shared/constants";
+import { belongingFunctions } from "../../../shared/constants";
 
 @Component({
   selector: 'application-evaluate-paramete-info',
@@ -15,57 +13,62 @@ import {belongingFunctions} from "../../../shared/constants";
   styleUrls: ['./application-evaluate-paramete-info.component.scss']
 })
 export class ApplicationEvaluationParameterInfoComponent implements OnInit {
-  submitted: boolean;
+  submitted = false;
   applicationId: string;
   parameter: Parameter;
+
+  protected readonly belongingFunctions = belongingFunctions;
 
   constructor(
     private messageService: MessageService,
     private parameterService: ParameterService,
     public dialogService: DialogService,
     public ref: DynamicDialogRef,
-    public config: DynamicDialogConfig) { }
+    public config: DynamicDialogConfig
+  ) {}
 
   ngOnInit(): void {
-    this.submitted = false;
-
     this.applicationId = this.config.data.applicationId;
-
-    if (this.config.data.parameter != null) {
-      this.parameter = this.config.data.parameter;
-    }
-    else {
-      this.parameter = new Parameter();
-    }
+    this.parameter = this.config.data.parameter ?? new Parameter();
   }
 
-  saveParameter() {
+  saveParameter(): void {
     this.submitted = true;
 
     if (this.parameter.id) {
-      this.parameterService.single.update(this.parameter).subscribe(
-        parameter => {
-          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Параметер оновлено' });
-          this.ref.close(parameter);
-        },
-        error => {
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
-        })
-    }
-    else {
-      this.parameter.id = UUID.UUID();
-      this.parameter.applicationId = this.config.data.applicationId;
-      this.parameterService.single.create(this.parameter).subscribe(
-        parameter => {
-          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Заявку створено' });
-          this.ref.close(parameter);
-        },
-        error => {
-          this.parameter.id = null;
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
-        })
+      this.updateParameter();
+    } else {
+      this.createParameter();
     }
   }
 
-  protected readonly belongingFunctions = belongingFunctions;
+  private updateParameter(): void {
+    this.parameterService.single.update(this.parameter).subscribe(
+      parameter => {
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Параметер оновлено' });
+        this.ref.close(parameter);
+      },
+      error => this.handleError(error)
+    );
+  }
+
+  private createParameter(): void {
+    this.parameter.id = UUID.UUID();
+    this.parameter.applicationId = this.config.data.applicationId;
+
+    this.parameterService.single.create(this.parameter).subscribe(
+      parameter => {
+        this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Заявку створено' });
+        this.ref.close(parameter);
+      },
+      error => {
+        this.parameter.id = null;
+        this.handleError(error);
+      }
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): void {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: String(error.error).split('\n')[0] });
+  }
 }
