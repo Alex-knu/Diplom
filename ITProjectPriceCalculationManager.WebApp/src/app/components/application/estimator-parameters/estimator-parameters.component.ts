fa17ece9bb4bf17c40b411e4b51083533ec567f3
Alex Rozdobudko <arozdobudko2@gmail.com>
2024-06-09 13:12:37 +0300

    create base view for competent form

--- ITProjectPriceCalculationManager.WebApp/src/app/components/application/estimator-parameters/estimator-parameters.component.ts
+++ ITProjectPriceCalculationManager.WebApp/src/app/components/application/estimator-parameters/estimator-parameters.component.ts
@@ -7,6 +7,8 @@ import { BaseApplicationService } from 'src/app/shared/services/api/baseApplicat
 import { ProgramLanguage } from "../../../shared/models/programLanguage.model";
 import { ProgramLanguageService } from "../../../shared/services/api/programLanguage.service";
 import { UUID } from "angular2-uuid";
+import {ParameterService} from "../../../shared/services/api/parameter.service";
+import {Parameter} from "../../../shared/models/parameter.model";
 
 @Component({
   selector: 'app-estimator-parameters',
@@ -15,61 +17,56 @@ import { UUID } from "angular2-uuid";
 })
 
 export class EstimatorParametersComponent implements OnInit {
-  submitted: boolean;
   application: BaseApplication;
   programLanguages: ProgramLanguage[];
+  parameters: Parameter[];
+  loading = false;
 
   constructor(
     private messageService: MessageService,
     private baseApplicationService: BaseApplicationService,
     private programLanguageService: ProgramLanguageService,
+    private parameterService: ParameterService,
     public dialogService: DialogService,
     public ref: DynamicDialogRef,
     public config: DynamicDialogConfig) { }
 
   ngOnInit(): void {
-    this.submitted = false;
-
-    this.programLanguageService.collection.getAll()
-      .subscribe(
-        (programLanguages) => {
-          this.programLanguages = programLanguages;
-        });
-
-    if (this.config.data != null) {
-      this.application = this.config.data;
-    }
-    else {
-      this.application = new BaseApplication();
-    }
+    this.parameterService.collection.getListById(this.config.data.id).subscribe(
+      parameters => {
+        this.parameters = parameters;
+        this.loading = false;
+      },
+      error => this.loading = false
+    );
   }
 
-  saveApplication() {
-    this.submitted = true;
-
-    if (this.application.id) {
-      this.baseApplicationService.single.update(this.application).subscribe(
-        application => {
-          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Заявку оновлено' });
-          this.ref.close(application);
-        },
-        error => {
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
-        })
-    }
-    else {
-      this.application.id = UUID.UUID();
-      this.application.price = 0;
-      this.application.statusId = "4706D234-E64D-4AB2-BED0-6086E10C3325";
-      this.baseApplicationService.single.create(this.application).subscribe(
-        application => {
-          this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Заявку створено' });
-          this.ref.close(application);
-        },
-        error => {
-          this.application.id = null;
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
-        })
-    }
+  calculateConfidenceArea() {
+    // this.submitted = true;
+    //
+    // if (this.application.id) {
+    //   this.baseApplicationService.single.update(this.application).subscribe(
+    //     application => {
+    //       this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Заявку оновлено' });
+    //       this.ref.close(application);
+    //     },
+    //     error => {
+    //       this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
+    //     })
+    // }
+    // else {
+    //   this.application.id = UUID.UUID();
+    //   this.application.price = 0;
+    //   this.application.statusId = "4706D234-E64D-4AB2-BED0-6086E10C3325";
+    //   this.baseApplicationService.single.create(this.application).subscribe(
+    //     application => {
+    //       this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Заявку створено' });
+    //       this.ref.close(application);
+    //     },
+    //     error => {
+    //       this.application.id = null;
+    //       this.messageService.add({ severity: 'error', summary: 'Error', detail: String((error as HttpErrorResponse).error).split('\n')[0] });
+    //     })
+    // }
   }
 }
